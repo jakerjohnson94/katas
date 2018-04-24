9431b42622c9a7a933c09f53994c2df3a7c28e9e
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-04-24 17:30:45 -0400

    all katas complete with only one for statement. #gangang

--- script.js
+++ script.js
@@ -1,68 +1,49 @@
-
 console.log("Display the numbers from 1 to 20 ");
-for (i=1; i<21; i++){
-    console.log(i);
+for (i = 1; i <= 20; i++) {
+  console.log(i);
 }
-
 
 console.log("Display the even numbers from 1 to 20. ");
-for(i=0; i<21; i+=2){
-
-        console.log(i);
-
+for (i = 0; i <= 20; i += 2) {
+  console.log(i);
 }
-
 
 console.log("Display the odd numbers from 1 to 20. ");
-for(i=1; i<21; i+=2){
-        console.log(i);
-    
+for (i = 1; i < 20; i += 2) {
+  console.log(i);
 }
 
- console.log("Display the multiples of 5 up to 100 ");
-for (i=5; i<101; i+=5){
-        console.log(i);
-    }
-
-
-
-console.log("Display the square numbers  up to 100. ");
-for(i=1; i<=10;i++){
-
-   console.log(i*i);
+console.log("Display the multiples of 5 up to 100 ");
+for (i = 5; i <= 100; i += 5) {
+  console.log(i);
 }
 
+console.log("Display the square numbers up to 100. ");
+for (i = 1; i <= 10; i++) {
+  console.log(i * i);
+}
 
 console.log("Display the numbers counting backwards from 20 to 1 ");
-for(i=20;i>0; i--){  console.log(i);
+for (i = 20; i > 0; i--) {
+  console.log(i);
 }
-
 
 console.log("Display the even numbers counting backwards from 20 ");
-for (i=20; i>0; i-=2){
-
-       console.log(i);
-    
+for (i = 20; i > 0; i -= 2) {
+  console.log(i);
 }
-
 
 console.log("Display the odd numbers from 20 to 1, counting backwards ");
-   for(i=19; i>0; i-=2){
-    
-           console.log(i);
-       
-   }
+for (i = 19; i > 0; i -= 2) {
+  console.log(i);
+}
 
 console.log("Display the multiples of 5, counting down from 100 ");
-for(i=100; i>0; i-=5){
-        console.log(i);
+for (i = 100; i > 0; i -= 5) {
+  console.log(i);
 }
-
-
 
 console.log("Display the square numbers, counting down from 100. ");
-for (i=10; i> 0 ; i--){
-    console.log(i*i);
+for (i = 10; i > 0; i--) {
+  console.log(i * i);
 }
-
-
