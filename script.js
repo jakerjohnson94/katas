3b12fea1773da402e53bef80ad125b9c749a37a9
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-04-24 14:43:24 -0400

    Simplified expressions

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 console.log("Display the numbers from 1 to 20 ");
 for (i=1; i<21; i++){
     console.log(i);
@@ -5,34 +6,30 @@ for (i=1; i<21; i++){
 
 
 console.log("Display the even numbers from 1 to 20. ");
-for(i=1; i<21; i++){
-    if((i%2) === 0){
+for(i=0; i<21; i+=2){
+
         console.log(i);
-    }
+
 }
 
 
 console.log("Display the odd numbers from 1 to 20. ");
-for(i=1; i<21; i++){
-    if((i%2) !== 0){
+for(i=1; i<21; i+=2){
         console.log(i);
-    }
+    
 }
 
  console.log("Display the multiples of 5 up to 100 ");
-for (i=1; i<101; i++){
-   if((i%5)===0){
+for (i=5; i<101; i+=5){
         console.log(i);
     }
-}
+
 
 
 console.log("Display the square numbers  up to 100. ");
-for(i=2; i<101;i++){
+for(i=2; (i*i)<101;i++){
     var sqr = i*i;
- if (sqr<101){
    console.log(sqr);
- }
 }
 
 
@@ -42,32 +39,30 @@ for(i=20;i>0; i--){  console.log(i);
 
 
 console.log("Display the even numbers counting backwards from 20 ");
-for (i=20; i>0; i--){
-    if((i%2)===0){
+for (i=20; i>0; i-=2){
+
        console.log(i);
-    }
+    
 }
 
 
 console.log("Display the odd numbers from 20 to 1, counting backwards ");
-   for(i=20; i>0; i--){
-       if((i%2)!==0){
+   for(i=19; i>0; i-=2){
+    
            console.log(i);
-       }
+       
    }
 
 console.log("Display the multiples of 5, counting down from 100 ");
-for(i=100; i>0; i--){
-    if((i%5)===0){
+for(i=100; i>0; i-=5){
         console.log(i);
-    }
 }
 
 
 
 console.log("Display the square numbers, counting down from 100. ");
 for(i=100; i>0; i--){
-    if((i*i)<101){
+    if((i*i<100)){
         console.log((i*i));
     }
 }
