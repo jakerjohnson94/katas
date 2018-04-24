8f78cd01ef6421fc4dc2819ddec4c97c73d82664
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-04-24 15:51:35 -0400

    blank template for revision

--- script.js
+++ script.js
@@ -1,69 +1,30 @@
 
 console.log("Display the numbers from 1 to 20 ");
-for (i=1; i<21; i++){
-    console.log(i);
-}
-
 
 console.log("Display the even numbers from 1 to 20. ");
-for(i=0; i<21; i+=2){
-
-        console.log(i);
-
-}
 
 
 console.log("Display the odd numbers from 1 to 20. ");
-for(i=1; i<21; i+=2){
-        console.log(i);
-    
-}
+
 
  console.log("Display the multiples of 5 up to 100 ");
-for (i=5; i<101; i+=5){
-        console.log(i);
-    }
-
 
 
 console.log("Display the square numbers  up to 100. ");
-for(i=2; (i*i)<101;i++){
-    var sqr = i*i;
-   console.log(sqr);
-}
+
 
 
 console.log("Display the numbers counting backwards from 20 to 1 ");
-for(i=20;i>0; i--){  console.log(i);
-}
 
 
 console.log("Display the even numbers counting backwards from 20 ");
-for (i=20; i>0; i-=2){
-
-       console.log(i);
-    
-}
 
 
 console.log("Display the odd numbers from 20 to 1, counting backwards ");
-   for(i=19; i>0; i-=2){
-    
-           console.log(i);
-       
-   }
 
 console.log("Display the multiples of 5, counting down from 100 ");
-for(i=100; i>0; i-=5){
-        console.log(i);
-}
 
 
 
 console.log("Display the square numbers, counting down from 100. ");
-for(i=100; i>0; i--){
-    if((i*i<100)){
-        console.log((i*i));
-    }
-}
 
